4eabdda6ac063bbbcae5d01d1dc45562ca8f0915
faramendi <faramendi@d31419fb-652e-0410-9cc2-a579b616d322>
2007-08-19 17:18:54 +0000

    refactor rompe todo

--- trunk/ar.com.bic.domain/esquema/Catalogo.cs
+++ trunk/ar.com.bic.domain/esquema/Catalogo.cs
@@ -4,23 +4,28 @@ using System.Collections;
 namespace ar.com.bic.domain.esquema
 {
 	/// <summary>
-	/// Descripción breve de Esquema.
+	/// Estructura de la Base de Datos. Contiene solo la colección de tablas
+	/// que figuran en la metadata de la BD
 	/// </summary>
-	public class Esquema
+	public class Catalogo
 	{
 		private IList tablas = new ArrayList();
+
+		/// <summary>
+		/// Tablas existentes en la BD
+		/// </summary>
 		public IList Tablas
 		{
 			get {return new ArrayList(this.tablas); }
 		}
 
-		public Esquema()
-		{
-		}
+		public Catalogo() {}
 
 		public void AgregarTabla(Tabla tbl)
 		{
 			this.tablas.Add(tbl);
 		}
+
+
 	}
 }
