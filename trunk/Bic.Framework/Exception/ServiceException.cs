dd044ef8f8519b56ea94cb2a48d8463a545a4386
faramendi <faramendi@d31419fb-652e-0410-9cc2-a579b616d322>
2007-09-18 22:26:27 +0000

    refactor namspaces rompetutttti

---
+++ trunk/Bic.Framework/Exception/ServiceException.cs
@@ -0,0 +1,15 @@
+namespace Bic.Framework.Exception
+{
+	/// <summary>
+	/// Descripción breve de ServiceException.
+	/// </summary>
+	public class ServiceException
+	{
+		public ServiceException()
+		{
+			//
+			// TODO: agregar aquí la lógica del constructor
+			//
+		}
+	}
+}
