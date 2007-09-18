907999a5080b6f04e30b1bbf7b8e29f3a45cc974
faramendi <faramendi@d31419fb-652e-0410-9cc2-a579b616d322>
2007-09-18 22:26:27 +0000

    refactor namspaces rompetutttti

---
+++ Bic.Framework/Aspects/LoggingAdvice.cs
@@ -0,0 +1,75 @@
+#region Imports
+
+using System;
+using AopAlliance.Intercept;
+using log4net;
+
+#endregion
+
+namespace Bic.Framework.Aspects
+{
+	public class LoggingAdvice : IMethodInterceptor
+	{
+		#region Logging
+
+		private static readonly ILog LOG = LogManager.GetLogger(typeof(LoggingAdvice));
+
+		#endregion
+
+		#region Members
+
+		private string level = null;
+
+		#endregion
+
+		#region Properties
+
+		public string Level
+		{
+			get { return level; }
+			set { level = value; }
+		}
+
+		#endregion
+
+		#region IMethodInterceptor Members
+
+		public object Invoke(IMethodInvocation invocation)
+		{
+			Log(String.Format("Intercepted call : about to invoke method '{0}'", invocation.Method.Name));
+			object returnValue = invocation.Proceed();
+			Log(String.Format("Intercepted call : returned '{0}'", returnValue));
+			return returnValue;
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void Log(string text)
+		{
+			switch (level.ToLower())
+			{
+				case "debug":
+					if (LOG.IsDebugEnabled) LOG.Debug(text);
+					break;
+				case "error":
+					if (LOG.IsErrorEnabled) LOG.Error(text);
+					break;
+				case "fatal":
+					if (LOG.IsFatalEnabled) LOG.Fatal(text);
+					break;
+				case "info":
+					if (LOG.IsInfoEnabled) LOG.Info(text);
+					break;
+				case "warn":
+					if (LOG.IsWarnEnabled) LOG.Warn(text);
+					break;
+				default:
+					throw new ArgumentException(String.Format("Level '{0}' unknown.", level));
+			}
+		}
+
+		#endregion
+	}
+}
